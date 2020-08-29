3e118d2daa3a2f2bb99981287fed5f239d82bd64
Bernard828 <bernard.lackey1@gmail.com>
2020-08-28 23:14:48 -0400

    deleted player and song.js files; removed dead code in foodType component as well

--- frontend-spa/src/js/components/foodTypes.js
+++ frontend-spa/src/js/components/foodTypes.js
@@ -1,23 +1,4 @@
-// export default function FoodTypes(foodTypes) {
-//     return `
-//         <h1><u>Food Categories</h1></u>
-//         <h2>Click a food category below to view all of its recipes</h2>
-//         <section class="foodtype__list">
-//             ${foodTypes.map(foodType => {
-//                 return `
-//                     <div id="recipes"
-//                     <ul><article>
-//                         <a class="foodType__item">
-//                         <h2 class='foodType__category' id="${foodType.Id}">${foodType.foodCategory}</h2>
-//                         <img src="images/${foodType.foodImage}" alt="image" style="width: 100px; height: 100px">
-//                         </a>                 
-//                     </ul></article>
-//                     </div>
-//                 `
-//             }).join("")}
-//         </section>
-//     `
-// }
+
 export default function FoodTypes(foodTypes) {
     return `
     <h1><u>Food Types</h1></u>
